86dc8f96456729b68a456665392142bd59d18780
bhborkar91 <bhborkar91@github.com>
2017-08-24 22:33:17 +0530

    Added code for toggling visibility

--- app/components/answer-listing.js
+++ app/components/answer-listing.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  commentsVisible: true,
+  actions: {
+    toggleComments: function() {
+      this.set('commentsVisible', !this.get('commentsVisible'));
+    }
+  }
 });
--- app/components/question-listing.js
+++ app/components/question-listing.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  commentsVisible: true,
+  answersVisible: true,
+  actions: {
+    toggleComments: function() {
+      this.set('commentsVisible', !this.get('commentsVisible'));
+    },
+    toggleAnswers: function() {
+      this.set('answersVisible', !this.get('answersVisible'));
+    }
+
+  }
+
 });
